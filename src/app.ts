9d6b54a2652b5897c812a5b6cb35e108922b1d0f
kidusshoa <captainkevin2008@gmail.com>
2025-04-02 11:02:59 +0300

    route/users

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import adminRoutes from "./routes/admin";
 import authRoutes from "./routes/auth";
 import adminBusinessRoutes from "./routes/adminBusiness";
+import adminUserRoutes from "./routes/adminUser";
 import swaggerUi from "swagger-ui-express";
 import { swaggerSpec } from "./config/swagger";
 
@@ -14,5 +15,6 @@ app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 app.use("/api/admin", adminRoutes);
 app.use("/api/auth", authRoutes);
 app.use("/api/admin/businesses", adminBusinessRoutes);
+app.use("/api/admin/users", adminUserRoutes);
 
 export default app;
